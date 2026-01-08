7c328d0fcc064f8b0b0a84e8887371f6c0590e48
Marco Zank <zank@gmx.at>
2026-01-08 16:57:15 +0100

    docs: regenerate Doxygen HTML to include SpGEMM example and updated hipBLAS benchmark results

--- docs/html/search/all_7.js
+++ docs/html/search/all_7.js
@@ -1,8 +1,9 @@
 var searchData=
 [
-  ['hip_5fcheck_0',['hip_check',['../fftpoisson3d_8cpp_aaf41965393ffaedeec1441c5bc1c04e9.html#aaf41965393ffaedeec1441c5bc1c04e9',1,'HIP_CHECK:&#160;fftpoisson3d.cpp'],['../gemm_8cpp_aaf41965393ffaedeec1441c5bc1c04e9.html#aaf41965393ffaedeec1441c5bc1c04e9',1,'HIP_CHECK:&#160;gemm.cpp'],['../montecarlointegration_8cpp_aaf41965393ffaedeec1441c5bc1c04e9.html#aaf41965393ffaedeec1441c5bc1c04e9',1,'HIP_CHECK:&#160;montecarlointegration.cpp'],['../sorting_8cpp_aaf41965393ffaedeec1441c5bc1c04e9.html#aaf41965393ffaedeec1441c5bc1c04e9',1,'HIP_CHECK:&#160;sorting.cpp'],['../vectorreduction_8cpp_aaf41965393ffaedeec1441c5bc1c04e9.html#aaf41965393ffaedeec1441c5bc1c04e9',1,'HIP_CHECK:&#160;vectorreduction.cpp']]],
+  ['hip_5fcheck_0',['hip_check',['../fftpoisson3d_8cpp_aaf41965393ffaedeec1441c5bc1c04e9.html#aaf41965393ffaedeec1441c5bc1c04e9',1,'HIP_CHECK:&#160;fftpoisson3d.cpp'],['../gemm_8cpp_aaf41965393ffaedeec1441c5bc1c04e9.html#aaf41965393ffaedeec1441c5bc1c04e9',1,'HIP_CHECK:&#160;gemm.cpp'],['../montecarlointegration_8cpp_aaf41965393ffaedeec1441c5bc1c04e9.html#aaf41965393ffaedeec1441c5bc1c04e9',1,'HIP_CHECK:&#160;montecarlointegration.cpp'],['../sorting_8cpp_aaf41965393ffaedeec1441c5bc1c04e9.html#aaf41965393ffaedeec1441c5bc1c04e9',1,'HIP_CHECK:&#160;sorting.cpp'],['../spgemm_8cpp_ad32de5b8f658b6661897a0582187e5c8.html#ad32de5b8f658b6661897a0582187e5c8',1,'HIP_CHECK:&#160;spgemm.cpp'],['../vectorreduction_8cpp_aaf41965393ffaedeec1441c5bc1c04e9.html#aaf41965393ffaedeec1441c5bc1c04e9',1,'HIP_CHECK:&#160;vectorreduction.cpp']]],
   ['hipblas_5fcheck_1',['HIPBLAS_CHECK',['../gemm_8cpp_a2775524fd26fbcc4fcb729b926e089af.html#a2775524fd26fbcc4fcb729b926e089af',1,'gemm.cpp']]],
   ['hipfft_5fcheck_2',['HIPFFT_CHECK',['../fftpoisson3d_8cpp_a2381dfa0eab8155c67775aa5d11c46f0.html#a2381dfa0eab8155c67775aa5d11c46f0',1,'fftpoisson3d.cpp']]],
   ['hiprand_5fcheck_3',['HIPRAND_CHECK',['../sorting_8cpp_a990447f95230ebf6f4b21894ac069e6c.html#a990447f95230ebf6f4b21894ac069e6c',1,'sorting.cpp']]],
-  ['how_20to_20run_4',['How to Run',['../fftpoisson3d_8cpp.html#run',1,'']]]
+  ['hipsparse_5fcheck_4',['HIPSPARSE_CHECK',['../spgemm_8cpp_ac1608f7d72cd68bee2c1995f083ef81b.html#ac1608f7d72cd68bee2c1995f083ef81b',1,'spgemm.cpp']]],
+  ['how_20to_20run_5',['How to Run',['../fftpoisson3d_8cpp.html#run',1,'']]]
 ];
--- docs/html/search/files_4.js
+++ docs/html/search/files_4.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['sorting_2ecpp_0',['sorting.cpp',['../sorting_8cpp.html',1,'']]]
+  ['sorting_2ecpp_0',['sorting.cpp',['../sorting_8cpp.html',1,'']]],
+  ['spgemm_2ecpp_1',['spgemm.cpp',['../spgemm_8cpp.html',1,'']]]
 ];
